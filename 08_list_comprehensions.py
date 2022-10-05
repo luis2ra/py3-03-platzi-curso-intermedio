32c6591f4e38a15604004a61d554ba3e51df64a8
Luis <luis@blooz.pe>
2022-10-05 13:07:58 -0500

    List comprehensions

---
+++ 08_list_comprehensions.py
@@ -0,0 +1,19 @@
+def run():
+    print('hola mundo!')
+
+    for x in range(1, 101):
+        print('Para ', x, ', el cuadrado es:', x * x)
+
+    # [element for element in iterable if condition]
+    [print(x ** 2) for x in range(1,10)]
+
+    squares = []
+    squares = [i ** 2 for i in range(1, 101) if i % 3 != 0]
+    print(squares)
+
+    challenge = []
+    challenge = [i for i in range(1, 100000) if (i % 4 == 0 and i % 6 == 0 and i % 9 == 0)]
+    print(challenge)
+
+if __name__ == '__main__':
+        run()
