a9a8aadefb08363b282cd85ab82d7c1ec4a35a18
Luis <luis@blooz.pe>
2022-10-05 13:51:56 -0500

    Dictionary comprehensions

---
+++ 09_dict_comprehensions.py
@@ -0,0 +1,22 @@
+import math
+
+def run():
+    print('hola mundo!')
+    
+    # crear un diccionario, llave 1 al 100, valores los cubos respectivos
+    cubes = {}
+
+    for i in range(1, 101):
+        cubes[i] = i ** 3
+    # print(cubes)
+
+    # {key:value for key in iterable if condition}
+    challenge = {i: i ** 3 for i in range (1, 101) if i % 3 != 0}
+    # print(challenge)
+
+    # crear un diccionario, llave 1 al 1000, donde los valores son las raices cuadradas
+    challenge2 = {i: math.sqrt(i) for i in range (1, 1001)}
+    print(challenge2)
+
+if __name__ == '__main__':
+    run()
